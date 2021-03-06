acc2f17eb25aaaeeb5b794650929c3c3d3a4434e
Tanzeela <tanzeelafathima1999@gmail.com>
2021-03-06 22:49:13 +0530

    title

--- foodmagic/lib/main.dart
+++ foodmagic/lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
+        title: 'food magic',
         theme: ThemeData(
           // This is the theme of your application.
           //
