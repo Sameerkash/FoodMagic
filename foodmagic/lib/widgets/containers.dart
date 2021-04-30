214cbf5adf745955e3d17adbfb3b92eeda2e34a1
Sameer Kashyap <sameera.s.kashyap@gmail.com>
2021-04-30 14:01:06 +0530

    fix path

--- foodmagic/lib/widgets/containers.dart
+++ foodmagic/lib/widgets/containers.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-import '../../utils/extensions.dart';
+import '../utils/extensions.dart';
 
 class CartContainer extends StatelessWidget {
   final String leading;
--- foodmagic/lib/widgets/field.dart
+++ foodmagic/lib/widgets/field.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
-import '../../utils/extensions.dart';
+import '../utils/extensions.dart';
 
 class CustomTextField extends StatelessWidget {
   final String label;
