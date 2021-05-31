b4eaff4a605402a0b23f572a0d94235766c6a735
Parinita Vedantam <vedantampari@gmail.com>
2021-05-31 08:09:45 +0530

    Fix errors

--- foodmagic/lib/views/ar/ar.view.dart
+++ foodmagic/lib/views/ar/ar.view.dart
@@ -1,3 +1,5 @@
+// @dart = 2.9
+
 import 'package:arcore_flutter_plugin/arcore_flutter_plugin.dart';
 import 'package:flutter/material.dart';
 import 'package:vector_math/vector_math_64.dart' as vector;
